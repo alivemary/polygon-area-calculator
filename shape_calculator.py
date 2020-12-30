daaca8575f40f931238309d32edd96530730c68a
alivemary <alivemary@gmail.com>
2020-12-30 09:13:43 +0100

    Solution

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,53 @@
+import math
+
+
 class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * (self.height + self.width)
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** 0.5
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+        return "".join(["*" * self.width + "\n" for i in range(self.height)])
+
+    def get_amount_inside(self, shape):
+        return math.floor(self.height / shape.height) * math.floor(
+            self.width / shape.width
+        )
 
 
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side, side)
 
+    def __str__(self):
+        return f"Square(side={self.width})"
 
-class Square:
+    def set_side(self, side):
+        super().set_width(side)
+        super().set_height(side)
+
+    def set_width(self, width):
+        self.set_side(width)
+
+    def set_height(self, height):
+        self.set_side(height)
